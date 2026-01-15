81e79f56b33e6440feb0ea2ffccffde15d8c563e
gituser <danjee2009@gmail.com>
2026-01-15 09:39:55 +0900

    회원 활동 정보 뷰 작성

---
+++ LinkUP-iOS/LinkUP-iOS/Source/Share/Enum/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  LinkUP-iOS
+//
+//  Created by maple on 1/15/26.
+//
+
+enum Category: String, CaseIterable{
+    case all = "전체"
+    case code = "코드"
+    case shcool = "학교"
+    case project = "프로젝트"
+}
